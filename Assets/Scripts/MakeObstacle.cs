597ac6f409536c87c23bca84f1ae2e3321fc4eee
Chaelin Park <166609121+cherrynniii@users.noreply.github.com>
2026-03-03 17:32:48 +0900

    feat: 장애물 생성

---
+++ Assets/Scripts/MakeObstacle.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MakeObstacle : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    float timer = 0.0f;
+    public float timeDiff;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            int index = Random.Range(0, obstacles.Length);
+            GameObject newObstacle = Instantiate(obstacles[index]);
+            if (index == 0)
+            {
+                newObstacle.transform.position = new Vector3(0.56f, Random.Range(-3.94f, 0.88f), 0);
+            }
+            else if (index == 1)
+            {
+                newObstacle.transform.position = new Vector3(2.71f, Random.Range(1.46f, 5.06f), 0);
+            }
+            else
+            {
+                newObstacle.transform.position = new Vector3(-3.96f, Random.Range(0.5f, 4.37f), 0);
+            }
+            timer = 0.0f;
+            Destroy(newObstacle, 6.0f);
+        }
+    }
+}
