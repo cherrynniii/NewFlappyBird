b771a9a7acbe932b4b347ac76dbb7c1622cad4ee
Chaelin Park <166609121+cherrynniii@users.noreply.github.com>
2026-03-05 10:06:52 +0900

    feat: 스테이지 레벨 정보 PlayScene으로 넘기기 (PlayerPrefs 사용)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    private int stageId;
 
     // 게임 실행 후 인스턴스 초기화 (Start보다 한 단계 더 앞에서 수행)
     void Awake()
     {
         if (instance == null)
         {
-            instance = this;        // gameManager을 넣어줌
+            instance = this;
         }
+
+        // 스테이지 레벨 받기
+        stageId = PlayerPrefs.GetInt("StageId", 1);
+        Debug.Log("Current Stage ID: " + stageId);
+    }
+
+    public int GetStageId()
+    {
+        return stageId;
     }
 }
--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
-    public void Play()
+    public void Play(int stageId)
     {
+        // PlayerPrefs: Unity가 제공하는 간단한 저장 시스템
+        PlayerPrefs.SetInt("StageId", stageId);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("PlayScene");
     }
 }
